2b10eb0215f83bfc82ce6fe16640f83159aaf15b
Anirudh Jonnalagadda <jonnalagadda.anirudh@gmail.com>
2016-03-10 16:27:28 +0530

    included parameters in the initializers

--- rigid.py
+++ rigid.py
@@ -10,16 +10,8 @@ from pysph.solver.application import Application
 from pysph.solver.solver import Solver
 from collapsing_cylinders_2d import CollapsingCylinderGeometry
 
-# constants
-
-l = h = 26.0   # length, height of tank (cm)
-r = 0.5        # radius of cylinder (cm)
-rho0 = 2.7e-3  # density of cylinder (kg.cm-3)
-
-hdx = 1.0
-
 class CollapsingCylinders(Application):
-    def initialize(self,layers = 6):
+    def initialize(self,layers = 6,hdx = 1.0):
         self.layers = layers
  
     def create_particles(self):
@@ -27,9 +19,9 @@ class CollapsingCylinders(Application):
         return geometry.create_particles()
 
     def create_solver(self):
-        kernel = CubicSpline(dim=3)
+        kernel = CubicSpline(dim=2)
         integrator = EPECIntegrator(cylinder = RK2StepRigidBody())
-        solver = Solver(kernel=kernel, dim=2, integrator=integrator, tf=10, 
+        solver = Solver(kernel=kernel, dim=2, integrator=integrator, tf=2, 
                         dt=5e-3, adaptive_timestep=False )
         solver.set_print_freq(10)
         return solver
@@ -37,10 +29,10 @@ class CollapsingCylinders(Application):
     def create_equations(self):
         equations = [ 
             Group(equations=[
-                       BodyForce(dest='cylinder', sources=None, gy=-9.81),
+                       #BodyForce(dest='cylinder', sources=None, gy=-9.81),
               RigidBodyCollision(dest='cylinder', 
                                  sources=['cylinder', 'container'], 
-                                 k=2.0, d=3.0, eta=0.1, kt=1)]),
+                                 k=1.0, d=10, eta=0.1, kt=0.1)]),
             Group(equations=[RigidBodyMoments(dest='cylinder',sources=None)]),
             Group(equations=[RigidBodyMotion(dest='cylinder',sources=None)]),]
         return equations
