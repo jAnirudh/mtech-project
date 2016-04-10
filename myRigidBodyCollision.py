42bdbf86a5a51f02edee3113886e4165c161b853
Anirudh Jonnalagadda <jonnalagadda.anirudh@gmail.com>
2016-04-10 14:45:03 +0530

    introduced loops where possible

--- myRigidBodyCollision.py
+++ myRigidBodyCollision.py
@@ -10,12 +10,12 @@ class myRigidBodyCollision(Equation):
         r_s = sqrt(s_x[s_idx]**2 + s_y[s_idx]**2 + s_z[s_idx]**2)
         return (r_s*r_d / r_s+r_d)
     
-    def loop(self,d_idx,s_idx,d_E,s_E,d_nu,s_nu,d_cm,s_cm,HIJ,RIJ,VIJ,
+    def loop(self,d_idx,s_idx,d_E,s_E,d_nu,s_nu,s_mu,d_cm,s_cm,HIJ,RIJ,VIJ,
              d_x,d_y,d_z,s_x,s_y,s_z,d_fx,d_fy,d_fz,d_total_mass,s_total_mass,
              d_body_id, s_body_id):
         '''
         '''
-        # Figure out which among the possibly many bodies we are dealing with
+        # Figure out which among the (possibly) many bodies we are dealing with
         d_body , s_body = d_body_id[d_idx], s_body_id[s_idx]
 
         ##### Calculate Normal Forces using the Modified, Non-Linear, #####
@@ -32,9 +32,8 @@ class myRigidBodyCollision(Equation):
         DELTAIJ = max(0.,HIJ - RIJ)
         # Calculate the Unit Vector between the two Center of Masses
         EIJ = zeros_like(d_cm)
-        EIJ[0] = d_cm[0] - s_cm[0]
-        EIJ[1] = d_cm[1] - s_cm[1]
-        EIJ[2] = d_cm[2] - s_cm[2]
+        for component in range(3):
+            EIJ[component] = d_cm[component] - s_cm[component]
         # Normalize to obtain unit vector
         EIJ =  EIJ / sqrt(EIJ[0]**2+EIJ[1]**2+EIJ[2]**2)
         # Calculate Rate of Normal Deformation, DELTADOTIJ
@@ -43,9 +42,9 @@ class myRigidBodyCollision(Equation):
         FnIJ = numpy.zeros(3)
         FnrIJ = k_n_ij*DELTAIJ**(3./2.)                # Normal Repulsive force
         FndIJ = gamma_n_ij*DELTAIJ**(1./4.)*DELTADOTIJ # Normal Damping force
-        FnIJ[0] = Eij[0] * ( FnrIJ - FndIJ )
-        FnIJ[1] = Eij[1] * ( FnrIJ - FndIJ )
-        FnIJ[2] = Eij[2] * ( FnrIJ - FndIJ )
+        for component in range(3):
+            FnIJ[component] = Eij[component] * ( FnrIJ - FndIJ )
         
-        ### Calculate Normal Forces using the Modified, Non-Linear, ###
-        ###                     Hertzian Model                      ###
+        ### Calculate Contact Forces using the Modified Coulomb's Law ###
+        
+        
