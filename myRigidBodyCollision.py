88cb2c80c55937d6ab4cb7e05cc58958610d6aea
Anirudh Jonnalagadda <jonnalagadda.anirudh@gmail.com>
2016-04-20 14:46:31 +0530

    Removed all Cython related bugs; other minor fixes

--- myRigidBodyCollision.py
+++ myRigidBodyCollision.py
@@ -20,8 +20,8 @@ class myRigidBodyCollision(Equation):
         nuI, nuJ = d_nu[d_idx], s_nu[s_idx]
         muI, muJ = d_mu[d_idx], s_mu[s_idx]
         MI, MJ = d_total_mass[d_body], s_total_mass[s_body]
-        Ri = (d_x[d_idx]**2 + d_y[d_idx]**2 + d_z[d_idx]**2)**0.5
-        Rj = (s_x[s_idx]**2 + s_y[s_idx]**2 + s_z[s_idx]**2)**0.5
+        Ri = sqrt(d_x[d_idx]**2 + d_y[d_idx]**2 + d_z[d_idx]**2)
+        Rj = sqrt(s_x[s_idx]**2 + s_y[s_idx]**2 + s_z[s_idx]**2)
 
         ##### Calculate Normal Forces using the Modified, Non-Linear, #####
         #####                    Hertzian Model                       #####
@@ -36,19 +36,26 @@ class myRigidBodyCollision(Equation):
         # Calculate Particle Overlap
         DELTAIJ = DELTATIJ = max(0.0,HIJ - RIJ)
         # Calculate the Unit Vector between the two Center of Masses
-        EIJ = zeros(3)
+        EIJ = declare('matrix((3,))')
+        s_cm_index = declare('int')
+        d_cm_index = declare('int')
         s_cm_index = 3*s_body
         d_cm_index = 3*d_body
+        E2IJ = 0.0
         for i in range(3):
             EIJ[i] = d_cm[d_cm_index+i] - s_cm[s_cm_index+i]
+            E2IJ += EIJ[i]**2
         # Normalize to obtain unit vector
-        EIJ =  EIJ / sqrt(EIJ[0]**2+EIJ[1]**2+EIJ[2]**2)
+        for i in range(3): 
+            EIJ[i] =  EIJ[i] / E2IJ
         # Calculate Rate of Normal Deformation, DELTADOTIJ
         DELTADOTIJ = VIJ[0]*EIJ[0] + VIJ[1]*EIJ[1] + VIJ[2]*EIJ[2]
         # Calculate Normal Contact Force, FnIJ
         FnrIJ = k_n_ij*DELTAIJ**(1.5)                 # mag of Repulsive force
         FndIJ = gamma_n_ij*DELTAIJ**(0.25)*DELTADOTIJ # mag of Damping force
-        FnIJ = EIJ * (FnrIJ - FndIJ)
+        FnIJ = declare('matrix((3,))')
+        for i in range(3):
+            FnIJ[i] = EIJ[i] * (FnrIJ - FndIJ)
         
         ####               Calculate Tangential Forces                ####
         
@@ -56,18 +63,22 @@ class myRigidBodyCollision(Equation):
         k_t_ij = 2./7. * k_n_ij
         gamma_t_ij = 2./7. * gamma_n_ij
         # Calculate Coefficient of friction, mu_ij
-        mu_f_ij = 0.5*(d_mu+s_mu)
+        mu_f_ij = 0.5*(muI+muJ)
         # Calculate Unit Vector ETIJ
-        ETIJ = VIJ - (DELTADOTIJ * EIJ)
+        ETIJ = declare('matrix((3,))')
+        for i in range(3):
+            ETIJ[i] = VIJ[i] - (DELTADOTIJ * EIJ[i])
         # Calculate Rate of Tangential Deformation, DELTATDOTIJ
         DELTATDOTIJ = VIJ[0]*ETIJ[0] + VIJ[1]*ETIJ[1] + VIJ[2]*ETIJ[2]
         # Calculate Tangential Contact Force
         FtrIJ = k_t_ij*DELTATIJ                 # mag of repulsive force
         FtdIJ = gamma_t_ij*DELTATIJ*DELTATDOTIJ # mag of damping force
         # Modified Coulomb Friction Force using the Sigmoid Function, Fc
-        Fc = mu_f_ij*(FnrIJ-FndIJ)*tanh(8*DELTATDOT)
+        Fc = mu_f_ij*(FnrIJ-FndIJ)*tanh(8*DELTATDOTIJ)
         # Calculate Tangential Contact Force
-        FtIJ = min(Fc,(FtrIJ-FdrIJ)) * ETIJ
+        FtIJ = declare('matrix((3,))')
+        for i in range(3):
+            FtIJ[i] = min(Fc,(FtrIJ-FtdIJ)) * ETIJ[i]
 
         ####               Total Contact Forces                       ####
 
